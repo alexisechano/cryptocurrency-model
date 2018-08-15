0abbd7426b4d58a3ada08b0d1b55c28b81b4ed92
alexis echano <alexis.echano@gmail.com>
2018-08-15 10:36:04 -0400

    Add files via upload

---
+++ trialOne.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from skimage import transform
+from skimage import data
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+from skimage.color import rgb2gray
+import random
+
