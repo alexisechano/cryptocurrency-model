e786c29b200f4c5fb2319abe0da091e961467a78
alexis echano <16446126+alexisechano@users.noreply.github.com>
2018-08-20 00:25:53 -0400

    Delete trialOne.py

--- trialOne.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from skimage import transform
-from skimage import data
-import matplotlib.pyplot as plt
-import os
-import numpy as np
-from skimage.color import rgb2gray
-import random
-
